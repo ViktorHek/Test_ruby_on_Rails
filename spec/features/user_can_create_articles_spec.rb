a65b7cfbf99a6531859a81c715aeea487b142493
viktor <viktorkarlsson125@gmail.com>
2020-11-10 14:54:14 +0100

    creating test: user can create article

---
+++ spec/features/user_can_create_articles_spec.rb
@@ -0,0 +1,23 @@
+feature 'User can create articles' do
+    before do
+        visit root_path
+        click_on "New Article"
+    end
+
+    context "Successfully create an article [Happy Path]" do
+        before do
+            fill_in "Title", with: "Happy holidays"
+            fill_in "Content", with: "Buy your gifts now!"
+            click_on "Create Article"
+        end
+
+        it 'User should be on article show page' do
+            article = Article.find_by(title: 'Happy holidays')
+            expect(corrent_path).to eq article_path(article)
+        end
+
+        it 'User should see article title' do
+            expect(page).to have_content 'Buy your gifts now!'
+        end
+    end
+end
