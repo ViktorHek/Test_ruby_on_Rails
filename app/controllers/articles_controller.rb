da09d7174ff0d8515b8c537ff3de98d3130a439f
viktor <viktorkarlsson125@gmail.com>
2020-11-14 22:07:31 +0100

    adding test jurnalist_can_edit. adding action edit and update. adding edit.html

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,19 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article, notice: 'Article was successfully updated'
+    else 
+      redirect_to edit_article_path, notice: 'Something went wrong, try again'
+    end
+  end
+
   private 
 
   def article_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   root controller: :articles, action: :index
 
-  resources :articles, only: [:show, :new, :create]
+  resources :articles, only: [:show, :new, :create, :edit, :update]
   
 end
